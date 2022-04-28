8a8f1349247db4d2faa0a83bdd295a6e87156d54
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-28 22:36:56 +0200

    Create Command.php

---
+++ src/LDFx/ItsToxicGG/LDCommand/Command.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LDFx\ItsToxicGG\LDCommand;
+// pocketmine
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+// LDFx
+use LDFx\ItsToxicGG\LDForm\Form;
+
+class Command extends CommandSender{
+  // soon!
+}
