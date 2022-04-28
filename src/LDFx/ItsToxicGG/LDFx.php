8beeb03d2361e997de47a5702423d81fe0453448
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-28 23:17:13 +0200

    Update LDFx.php

--- src/LDFx/ItsToxicGG/LDFx.php
+++ src/LDFx/ItsToxicGG/LDFx.php
@@ -4,7 +4,7 @@ namespace LDFx\ItsToxicGG;
 
 // LDFX
 use LDFx\ItsToxicGG\LDForm\Form;
-use LDFx\ItsToxicGG\LDCommand\FXCommand;
+use LDFx\ItsToxicGG\LDCommand\Command;
 use LDFx\ItsToxicGG\EventListener;
 // POCKETMINE
 use pocketmine\plugin\PluginBase;
