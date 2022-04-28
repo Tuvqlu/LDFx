2064b4cc00c59fd1059a46ecc0eac22427caf7a2
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-28 23:16:54 +0200

    Update and rename FXCommand.php to Command.php

--- src/LDFx/ItsToxicGG/LDCommand/Command.php
+++ src/LDFx/ItsToxicGG/LDCommand/Command.php
@@ -7,7 +7,7 @@ use pocketmine\command\Command;
 // LDFx
 use LDFx\ItsToxicGG\LDForm\Form;
 
-class FXCommand extends Command{
+class Command extends Command{
   
  public function __construct(Form $form){
    $this->form = $form;
