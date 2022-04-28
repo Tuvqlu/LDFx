625a8a7c87a36b3ef4c030c14365e64160609511
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-28 23:04:17 +0200

    implement commands

--- src/LDFx/ItsToxicGG/LDCommand/Command.php
+++ src/LDFx/ItsToxicGG/LDCommand/Command.php
@@ -8,5 +8,24 @@ use pocketmine\command\Command;
 use LDFx\ItsToxicGG\LDForm\Form;
 
 class Command extends CommandSender{
-  // soon!
+  
+ public function __construct(Form $form){
+   $this->form = $form;
+ }
+  
+ public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+   switch($cmd->getName() === "settings"){
+     case "ui":
+       if($sender->hasPermission("settings.cmd")){
+         if(!$sender instanceof Player){
+           $sender->sendMessage("This Command Only Works for players! Please perform this command IN GAME!");
+         }else{
+           $this->form->SettingsForm($sender);
+         }
+       }else{
+         $sender->sendMessage("You don't have permission to use this command");
+       }
+     break;
+  }
+  return true;
 }
