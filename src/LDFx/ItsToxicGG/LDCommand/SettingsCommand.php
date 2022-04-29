c5bb15fb635a87cae10f7c09b09f813bef910de3
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-29 14:39:10 +0200

    Update and rename Command.php to SettingsCommand.php

---
+++ src/LDFx/ItsToxicGG/LDCommand/SettingsCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace LDFx\ItsToxicGG\LDCommand;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\Plugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+
+use LDFx\ItsToxicGG\LDForm\Form;
+
+class SettingsCommand extends Command implements PluginOwned{
+    
+    private $plugin;
+    public $author = 'ItsToxicGG';
+
+    public function __construct(Form $plugin){
+        $this->plugin = $plugin;
+        
+        parent::__construct("settingsui", "§r§fYour Settings, Plugin By $author", "§cUse: /settings", ["settings"]);
+        $this->setPermission("settings.fx");
+        $this->setAliases(["settings"]);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(count($args) == 0){
+            if($sender instanceof Player) {
+                $this->plugin->SettingsForm($sender);
+            } else {
+                $sender->sendMessage("Use this command in-game");
+            }
+        }
+        return true;
+    }
+    
+    public function getPlugin(): Plugin{
+        return $this->plugin;
+    }
+
+    public function getOwningPlugin(): Loader{
+        return $this->plugin;
+    }
+}
