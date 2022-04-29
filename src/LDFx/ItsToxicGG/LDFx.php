e5a51cadc6b30c8e8862ee6d6db32e59ec79c88b
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-29 17:26:51 +0200

    .

--- src/LDFx/ItsToxicGG/LDFx.php
+++ src/LDFx/ItsToxicGG/LDFx.php
@@ -4,17 +4,21 @@ namespace LDFx\ItsToxicGG;
 
 // LDFX
 use LDFx\ItsToxicGG\LDForm\Form;
-use LDFx\ItsToxicGG\LDCommand\Command;
+use LDFx\ItsToxicGG\LDCommand\SettingsCommand;
+use LDFx\ItsToxicGG\LDCommand\FlyCommand;
 use LDFx\ItsToxicGG\EventListener;
 // POCKETMINE
 use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
+use pocketmine\Server;
 
 class LDFx extends PluginBase
 {
   
   public function onEnable(): void{
       $this->getLogger()->info("Enabled Plugin");
+      $this->getServer()->getCommandMap()->register("settings", new SettingsCommand($this));
+      $this->getServer()->getCommandMap()->register("fly", new FlyCommand($this));
   }
   
   public function onDiable(): void{
