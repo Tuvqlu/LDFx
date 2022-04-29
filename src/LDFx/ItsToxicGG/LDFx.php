14244a8802d7bcebd0a41847179b8fc52d15e9ea
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-29 18:06:39 +0200

    moved form to plugin

--- src/LDFx/ItsToxicGG/LDFx.php
+++ src/LDFx/ItsToxicGG/LDFx.php
@@ -3,7 +3,6 @@
 namespace LDFx\ItsToxicGG;
 
 // LDFX
-use LDFx\ItsToxicGG\LDForm\Form;
 use LDFx\ItsToxicGG\LDCommand\SettingsCommand;
 use LDFx\ItsToxicGG\LDCommand\FlyCommand;
 use LDFx\ItsToxicGG\EventListener;
@@ -11,6 +10,9 @@ use LDFx\ItsToxicGG\EventListener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\player\Player;
 use pocketmine\Server;
+// FORM
+use Vecnavium\FormsUI\CustomForm;
+use Vecnavium\FormsUI\SimpleForm;
 
 class LDFx extends PluginBase
 {
@@ -24,4 +26,56 @@ class LDFx extends PluginBase
   public function onDiable(): void{
       $this->getLogger()->info("Disabled Plugin");
   }
+  
+  public function SettingsForm($player){
+       $form = new SimpleForm(function(Player $player, int $data = null){
+            if($data === null){
+                return true;
+            }
+            switch($data){
+                case 0:
+                    $this->FlyForm($player);
+                    $player->sendMessage("§aYou Have Left the Settings to FlyForm!");
+                break;
+            
+                case 1:
+                    $player->sendMessage("§aYou Have Left The Form!");
+                break;
+            
+            }
+       });
+       $form->setTitle("§bSettings");
+       $form->setContent("§fPick THe Setting!");
+       $form->setButton("§aFly§cSettings");
+       $form->setButton("§cEXIT");
+       $form->sendToPlayer($player);
+       return $form;
+  }
+  
+  public function FlyForm($player){
+      $form = new CustomForm(function(Player $player, $data){
+          if($data === null){
+              return true;
+          }
+          switch($data){
+              case true:
+                  $player->setFlying(true);
+                  $player->setAllowFlight(true);
+                  $player->sendMessage("§aFly Is Active");
+              break;
+            
+              case false:
+                  $player->setFlying(false);
+                  $player->setAllowFlight(true);
+                  $player->sendMessage("§cFly Is Disabled");
+              break;
+           }
+      });
+      $form->setTitle("§aFly§cSettings");
+      $form->setLabel("§fChoose if you want fly to be off or on");
+      $form->setToggle("§fFly", false);
+      $form->sendToPlayer($player);
+      return $form;
+  }
 }
+ 
