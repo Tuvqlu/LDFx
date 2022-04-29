6150f3dfe388d9f3348d1c064f4588ba52b9ad3b
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-29 20:58:54 +0200

    Update NickColorCommand.php

--- src/LDFx/ItsToxicGG/LDCommand/NickColorCommand.php
+++ src/LDFx/ItsToxicGG/LDCommand/NickColorCommand.php
@@ -1,1 +1,45 @@
+<?php
 
+namespace LDFx\ItsToxicGG\LDCommand;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\Plugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+
+use LDFx\ItsToxicGG\LDFx;
+
+class NickColorCommand extends Command implements PluginOwned{
+    
+    private $plugin;
+
+    public function __construct(LDFx $plugin){
+        $this->plugin = $plugin;
+        
+        parent::__construct("nickcolor", "§r§fYour NickNameColor Settings, Plugin By ItsToxicGG", "§cUse: /fly", ["nickcolor"]);
+        $this->setPermission("nickcolor.fx");
+        $this->setAliases(["nc"]);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(count($args) == 0){
+            if($sender instanceof Player) {
+                $this->plugin->NickColorForm($sender);
+            } else {
+                $sender->sendMessage("Use this command in-game");
+            }
+        }
+        return true;
+    }
+    
+    public function getPlugin(): Plugin{
+        return $this->plugin;
+    }
+
+    public function getOwningPlugin(): LDFx{
+        return $this->plugin;
+    }
+}
