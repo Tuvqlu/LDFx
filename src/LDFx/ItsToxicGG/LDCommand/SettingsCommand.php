9c6ab873d974a5a211b768bbc234800cbfde2984
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-29 18:18:38 +0200

    Update SettingsCommand.php

--- src/LDFx/ItsToxicGG/LDCommand/SettingsCommand.php
+++ src/LDFx/ItsToxicGG/LDCommand/SettingsCommand.php
@@ -10,14 +10,14 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginOwned;
 
-use LDFx\ItsToxicGG\LDForm\Form;
+use LDFx\ItsToxicGG\LDFx;
 
 class SettingsCommand extends Command implements PluginOwned{
     
-    private $form;
+    private $plugin;
 
-    public function __construct(Form $form){
-        $this->form = $form;
+    public function __construct(LDFx $plugin){
+        $this->plugin = $plugin;
         
         parent::__construct("settingsui", "§r§fYour Settings, Plugin By ItsToxicGG", "§cUse: /settings", ["settings"]);
         $this->setPermission("settings.fx");
@@ -27,11 +27,20 @@ class SettingsCommand extends Command implements PluginOwned{
     public function execute(CommandSender $sender, string $commandLabel, array $args){
         if(count($args) == 0){
             if($sender instanceof Player) {
-                $this->form->SettingsForm($sender);
+                $this->plugin->SettingsForm($sender);
             } else {
                 $sender->sendMessage("Use this command in-game");
             }
         }
         return true;
     }
+    
+    public function getPlugin(): Plugin{
+        return $this->plugin;
+    }
+
+    public function getOwningPlugin(): LDFx{
+        return $this->plugin;
+    }
+}
 }
