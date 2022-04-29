34152de86cd4500f0cec4a81a58ad0139074fcf7
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-04-29 21:08:51 +0200

    Update EventListener.php

--- src/LDFx/ItsToxicGG/EventListener.php
+++ src/LDFx/ItsToxicGG/EventListener.php
@@ -6,9 +6,5 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 
 class EventListener implements Listener{
-  
-	public function onJoin(PlayerJoinEvent $event){
-        $player = $event->getPlayer();
-        $event->setJoinMessage("Thanks For Installing LDFx By ItsToxicGG");
-  }
+   // closed!
 }
