44104829b1e34e67788bd1976a171f9e3b39b941
ItsToxicGGAlt <104733482+ItsToxicGGAlt@users.noreply.github.com>
2022-05-01 21:18:30 +0200

    Forgotten command when 1.5 was in-development second 1

---
+++ src/LDFx/ItsToxicGG/LDCommand/SocialMenuCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LDFx\ItsToxicGG\LDCommand;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\Plugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+
+use LDFx\ItsToxicGG\LDFx;
+
+class FlyCommand extends Command implements PluginOwned{
+    
+    private $plugin;
+
+    public function __construct(LDFx $plugin){
+        $this->plugin = $plugin;
+        
+        parent::__construct("socialmenu", "§r§fYour Social Menu, Plugin By ItsToxicGG", "§cUse: /socialmenu", ["socialmenu"]);
+        $this->setAliases(["sm"]);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        if(count($args) == 0){
+            if($sender instanceof Player) {
+                $this->plugin->SocialMenuForm($sender);
+            } else {
+                $sender->sendMessage("Use this command in-game");
+            }
+        }
+        return true;
+    }
+    
+    public function getPlugin(): Plugin{
+        return $this->plugin;
+    }
+
+    public function getOwningPlugin(): LDFx{
+        return $this->plugin;
+    }
+}
