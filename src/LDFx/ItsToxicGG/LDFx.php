ebe5cdc7e1426795ab9600bc475295addec5e16d
ItsToxicGG <95104544+ItsToxicGG@users.noreply.github.com>
2022-05-01 18:22:06 +0200

    Update LDFx.php

--- src/LDFx/ItsToxicGG/LDFx.php
+++ src/LDFx/ItsToxicGG/LDFx.php
@@ -30,25 +30,12 @@ class LDFx extends PluginBase implements Listener
   public function onEnable(): void{
       $this->getLogger()->info("§aEnabled LDFx");
       $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      $this->BetterPearl();
       @mkdir($this->getDataFolder());
       $this->saveDefaultConfig();
       $this->getServer()->getCommandMap()->register("settings", new SettingsCommand($this));
       $this->getServer()->getCommandMap()->register("fly", new FlyCommand($this));
       $this->getServer()->getCommandMap()->register("nickcolor", new NickColorCommand($this));
-      $this->getServer()->getPluginManager()->registerEvent(ProjectileHitEvent::class, static function (ProjectileHitEvent $event) : void{
-            $projectile = $event->getEntity();
-            $entity = $projectile->getOwningEntity();
-            if ($projectile instanceof EnderPearl and $entity instanceof Player) {
-                $vector = $event->getRayTraceResult()->getHitVector();
-                (function() use($vector) : void{
-                    $this->setPosition($vector);
-                })->call($entity);
-                $location = $entity->getLocation();
-                $entity->getNetworkSession()->syncMovement($location, $location->yaw, $location->pitch);
-                $projectile->setOwningEntity(null);
-            }
-        }, EventPriority::NORMAL, $this);
-    }
   }
   
   public function onDiable(): void{
@@ -215,5 +202,21 @@ class LDFx extends PluginBase implements Listener
                break;
        }
   }
-}   
+
+  public function BetterPearl(){
+       $this->getServer()->getPluginManager()->registerEvent(ProjectileHitEvent::class, static function (ProjectileHitEvent $event) : void{
+           $projectile = $event->getEntity();
+           $entity = $projectile->getOwningEntity();
+           if ($projectile instanceof EnderPearl and $entity instanceof Player) {
+               $vector = $event->getRayTraceResult()->getHitVector();
+               (function() use($vector) : void{ //HACK : Closure bind hack to access inaccessible members
+                   $this->setPosition($vector);
+               })->call($entity);
+               $location = $entity->getLocation();
+               $entity->getNetworkSession()->syncMovement($location, $location->yaw, $location->pitch);
+               $projectile->setOwningEntity(null);
+           }
+       }, EventPriority::NORMAL, $this);
+   }
+}        
  
